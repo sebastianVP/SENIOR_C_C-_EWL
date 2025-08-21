c3d2ea33019e0892f2d5e9dcab88e7c37ab588ce
sebastianVP <alex.valdezp22@gmail.com>
2025-08-21 17:41:09 -0500

    DIA01

---
+++ semana1/dia01/paso1.md
@@ -0,0 +1,7 @@
+# **PASO 1: Repaso rapido de C**
+
+En C trabajamos mucho con:
+* punteros    -> variables que almacenan direcciones en memoria
+* malloc/free -> para reservar y liberar memoria
+* struct      -> para agrupar datos
+* stdio.h     -> entrada/salida estándar(printf,scaf,fgets,etc).
---
+++ semana1/dia01/promedio_max.c
@@ -0,0 +1,13 @@
+/*
+EJERCICIO 1:
+Vamos a crear un pgorama en C que:
+1. Pida al usuario cuantos numeros quiere ingresar.
+2. Reserve memoria dinámicamente para almacenarlos.
+3. Calcule el promedio y el máximo
+4. Libere memoria antes de terminar.
+*/
+
+# include <stdio.c>
+# include <stdlib.h>
+
+int  main(){}

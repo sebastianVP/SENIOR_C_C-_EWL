c26b9d68b8c038c7d3f047b20a1f004d0be07ef3
sebastianVP <alex.valdezp22@gmail.com>
2025-08-22 15:56:27 -0500

    example

--- semana1/dia01/promedio_max.c
+++ semana1/dia01/promedio_max.c
@@ -10,4 +10,32 @@ Vamos a crear un pgorama en C que:
 # include <stdio.c>
 # include <stdlib.h>
 
-int  main(){}
+int  main(){
+	int n,i;
+        float *numeros;
+        float suma=0,maximo;
+	printf("¿Cuantos numeros deseas ingresar?");
+        scanf("%d",&n);
+
+	//reservar memoria
+        numeros= (float*) malloc(n * sizeof(float));
+        if(numeros==NULL){
+		printf("Error al reservar memoria. \n");
+                return 1;
+	}
+        //LEER NUMERO
+        for(i=0;i<n;i++){
+	   printf("Numero %d:",i+1);
+           scanf("%f",&numeros[i]);
+           suma+=numeros[i];
+           if(i==0||numeros[i]>maximo){
+              maximo = numeros[i]
+           }
+	}
+        //Resultado
+        printf("\nPromedio = %.2f\n", suma/n);
+        printf("Máximo = %.2f \n",maximo);
+	//LIBERAR MEMORIA
+	free(numeros);
+        return 0;
+}
